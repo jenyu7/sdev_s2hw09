af4d52a449b2a4df08a6e3bb277b9fea98fa85e6
jenyu7 <jyu7@stuy.edu>
2018-03-06 20:30:20 -0500

    works, but dblclick may not be intent

--- clickdraw.js
+++ clickdraw.js
@@ -10,13 +10,9 @@ var svg = document.getElementById("slate");
 var width = svg.getAttribute("width");
 var height = svg.getAttribute("height");
 
-var animate = document.getElementById("animate");
-var dvd_a = document.getElementById("dvd");
-var stop = document.getElementById("stop");
+var clear = document.getElementById("clear");
 
-console.log(animate);
-console.log(dvd_a);
-console.log(stop);
+console.log(clear);
 
 //initialized radius
 var r = 5;
@@ -24,12 +20,6 @@ var r = 5;
 var grow = true;
 //animation id
 var id = 0;
-
-//updates grow bool depending on radius
-var update = function(r){
-    if(r < 5 || r >= 400)
-	grow = !grow;
-};
 
 //clears the screen and resets the prevX and prevY variables
 var clearCallBack = function(e){
@@ -39,60 +29,46 @@ var clearCallBack = function(e){
 };
 
 //draws a dot, returns the x and y coordinates in a list
-var drawDot = function(){
-    stopA();
-    clearCallBack();
+var drawDot = function(e){
     var c = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-    c.setAttribute("cx", 300);
-    c.setAttribute("cy", 300);
-    c.setAttribute("r", r);
+    c.setAttribute("cx", e.offsetX);
+    c.setAttribute("cy", e.offsetY);
+    c.setAttribute("r", 10);
     c.setAttribute("fill", "red");
     c.setAttribute("stroke", "black");
+    c.addEventListener("click", changeColor, true);
+    c.addEventListener("dblclick", deleteCircle);
     svg.appendChild(c);
-    update(r);
-    if(grow)
-	r ++;
-    else
-	r --;
-    id = setInterval(drawDot, 8);
+    e.stopPropagation();
 };
 
-var img = new Image();
-img.src = "dvd.png";
-
-var dvdBounce = function(){
-    stopA();
-    clearCallBack();
-    //initializing
-    var x = Math.random() * (width - img.width);
-    var y = Math.random() * (height - img.height);
-    var c = document.createElementNS("http://www.w3.org/2000/svg", "image");
-    c.setAttribute("href", "dvd.png");
-    c.setAttribute("width", img.width);
-    c.setAttribute("height", img.height);
-    c.setAttribute("x", x);
-    c.setAttribute("y", y);
-    svg.appendChild(c);
-    var dx = 3 * Math.random() + 0.1;
-    var dy = 3 * Math.random() + 0.1;
-    var bounce = function(){
-	if (x < 0 || x > width - img.width)
-	    dx *= -1;
-	if (y < 0 || y > height - img.height)
-	    dy *= -1;
-	c.setAttribute("x", x+=dx);
-	c.setAttribute("y", y+=dy);
-    };
-    id = setInterval( bounce, 8 );
+//toggles the color between red and blue
+var changeColor = function(e){
+    if(this.getAttribute("fill") == "red"){
+        this.setAttribute("fill", "blue");
+    }
+    else{
+        this.setAttribute("fill", "red");
+    }
+    e.stopPropagation();
 }
 
-//stops the animation (id known through global var)
-var stopA = function(){
-    clearInterval(id);
-};
+//delete clicked circle on doubleclick
+var deleteCircle = function(e){
+    svg.removeChild(this);
+    var c = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+    c.setAttribute("cx", Math.random() * (width - 10));
+    c.setAttribute("cy", Math.random() * (height - 10));
+    c.setAttribute("r", 10);
+    c.setAttribute("fill", "red");
+    c.setAttribute("stroke", "black");
+    c.addEventListener("click", changeColor, true);
+    c.addEventListener("dblclick", deleteCircle);
+    svg.appendChild(c);
+    e.stopPropagation();
+}
 
 //add the event listeners
-animate.addEventListener("click", drawDot);
-dvd_a.addEventListener("click", dvdBounce);
-stop.addEventListener("click", stopA);
+svg.addEventListener("click", drawDot);
+clear.addEventListener("click", clearCallBack);
 
